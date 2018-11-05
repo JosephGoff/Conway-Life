10fe76966af39b8f9bddaca34415c3bf5d56d66c
Joseph Goff <40038953+JosephGoff@users.noreply.github.com>
2018-11-05 12:39:29 -0500

    Updated from Brython Server: 11/5/2018 12:39:27 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,103 @@
 """
 conway.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Joseph Goff
+Credit: 
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+#Rules of the game:
+    # 1). Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    # 2). Any live cell with two or three live neighbors lives on to the next generation.
+    # 3). Any live cell with more than three live neighbors dies, as if by overpopulation.
+    # 4). Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    
+#Guidelines:
+    # 1). There should be no explosive growth.
+    # 2). There should exist small initial patterns with chaotic, unpredictable outcomes.
+    # 3). There should be potential for von Neumann universal constructors.
+    # 4). The rules should be as simple as possible, whilst adhering to the above constraints.
+
+#forming the seed of the system
+
+class Game(object):
+
+    def __init__(self, state, infinite_board = True):
+
+        self.state = state
+        self.width = state.width
+        self.height = state.height
+        self.infinite_board = infinite_board
+
+    def step(self, count = 1):
+
+        for generation in range(count):
+
+            new_board = [[False] * self.width for row in range(self.height)]
+
+            for y, row in enumerate(self.state.board):
+                for x, cell in enumerate(row):
+                    neighbours = self.neighbours(x, y)
+                    previous_state = self.state.board[y][x]
+                    should_live = neighbours == 3 or (neighbours == 2 and previous_state == True)
+                    new_board[y][x] = should_live
+
+            self.state.board = new_board
+
+    def neighbours(self, x, y):
+
+        count = 0
+
+        for hor in [-1, 0, 1]:
+            for ver in [-1, 0, 1]:
+                if not hor == ver == 0 and (self.infinite_board == True or (0 <= x + hor < self.width and 0 <= y + ver < self.height)):
+                    count += self.state.board[(y + ver) % self.height][(x + hor) % self.width]
+
+        return count
+
+    def display(self):
+        return self.state.display()
+
+class State(object):
+
+    def __init__(self, positions, x, y, width, height):
+
+        active_cells = []
+
+        for y, row in enumerate(positions.splitlines()):
+            for x, cell in enumerate(row.strip()):
+                if cell == 'o':
+                    active_cells.append((x,y))
+
+        board = [[False] * width for row in range(height)]
+
+        for cell in active_cells:
+            board[cell[1] + y][cell[0] + x] = True
+
+        self.board = board
+        self.width = width
+        self.height = height
+
+    def display(self):
+
+        output = ''
+
+        for y, row in enumerate(self.board):
+            for x, cell in enumerate(row):
+                if self.board[y][x]:
+                    output += ' o'
+                else:
+                    output += ' .'
+            output += '\n'
+
+        return output
+
+glider = """ oo.
+             o.o
+             o.. """
+
+my_game = Game(State(glider, x = 2, y = 3, width = 10, height = 10))
+print(my_game.display())
+my_game.step(27) 
+print(my_game.display())
+
